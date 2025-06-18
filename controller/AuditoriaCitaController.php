f61c405973533efae9c25b58bc95a745cc55e5dc
Hazel0722 <karinamora97@gmail.com>
2025-06-17 20:43:10 -0600

    auditoriaCitaControler

--- controller/AuditoriaCitaController.php
+++ controller/AuditoriaCitaController.php
@@ -12,12 +12,27 @@ class AuditoriaCitaController {
 
     // 🔍 Obtener todas las auditorías
     public function obtenerTodos() {
-        return $this->dao->obtenerTodos();
+        try {
+            return $this->dao->obtenerTodos();
+        } catch (Exception $e) {
+            error_log('Error en AuditoriaCitaController::obtenerTodos - ' . $e->getMessage());
+            return [];
+        }
     }
 
     // 🔍 Obtener una auditoría por ID
     public function obtenerPorId($id) {
-        return $this->dao->obtenerPorId($id);
+        if (!is_numeric($id)) {
+            error_log("ID inválido: $id");
+            return null;
+        }
+
+        try {
+            return $this->dao->obtenerPorId($id);
+        } catch (Exception $e) {
+            error_log('Error en AuditoriaCitaController::obtenerPorId - ' . $e->getMessage());
+            return null;
+        }
     }
 }
 
